0814fb9184c6a328b0f4379a525112c2a3f78241
rifal-prasetyo <hrdsdms765@gmail.com>
2025-05-26 20:27:58 +0700

    feat: RefreshAuthenticationUseCase

---
+++ src/Applications/use_case/_test/RefreshAuthenticationUseCase.test.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-undef */
+const RefreshAuthenticationUseCase = require('../RefreshAuthenticationUseCase');
+const AuthenticationTokenManager = require('../../security/AuthenticationTokenManager');
+const AuthenticationRepository = require('../../../Domains/authentications/AuthenticationRepository');
+const RefreshToken = require('../../../Domains/authentications/entities/RefreshToken');
+
+describe('RefreshAuthenticationUseCase', () => {
+  it('should orchestrating refresh authentication action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      refreshToken: 'refresh_token',
+    };
+    const { refreshToken } = new RefreshToken(useCasePayload);
+
+    /** creating depedency of use case */
+    const mockAuthenticationRepository = new AuthenticationRepository();
+    const mockAuthenticationTokenManager = new AuthenticationTokenManager();
+
+    /** mocking needed function */
+    mockAuthenticationRepository.verifyRefreshToken = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockAuthenticationTokenManager.verifyRefreshToken = jest.fn()
+      .mockImplementation(() => Promise.resolve({ id: 'user-123' }));
+    mockAuthenticationTokenManager.generateAccessToken = jest.fn()
+      .mockImplementation(() => Promise.resolve('access_token'));
+
+    /** creating use case instance */
+    const refreshAuthenticationUseCase = new RefreshAuthenticationUseCase({
+      authenticationRepository: mockAuthenticationRepository,
+      authenticationTokenManager: mockAuthenticationTokenManager,
+    });
+
+    // Action
+    const accessToken = await refreshAuthenticationUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(mockAuthenticationRepository.verifyRefreshToken)
+      .toHaveBeenCalled(refreshToken);
+    expect(mockAuthenticationTokenManager.verifyRefreshToken)
+      .toHaveBeenCalled(refreshToken);
+    expect(mockAuthenticationTokenManager.generateAccessToken)
+      .toHaveBeenCalled({ id: 'user-123' });
+    expect(accessToken).toStrictEqual('access_token');
+  });
+});
