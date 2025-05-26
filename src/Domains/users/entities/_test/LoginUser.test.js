7b0409fd52caa06ff131f4febf5159a893c5c383
rifal-prasetyo <hrdsdms765@gmail.com>
2025-05-26 20:23:01 +0700

    feat: LoginUser Entity

---
+++ src/Domains/users/entities/_test/LoginUser.test.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-undef */
+const LoginUser = require('../LoginUser');
+
+describe('a UserLogin entities', () => {
+  it('should throw error when payload not contain needed property', () => {
+    // Arrange
+    const payload = {
+      username: 'abc',
+    };
+
+    // Action and assert
+    expect(() => new LoginUser(payload)).toThrowError('LOGIN_USER.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet type data specification', () => {
+    // Arrange
+    const payload = {
+      username: 'abc',
+      password: ['true'],
+    };
+
+    expect(() => new LoginUser(payload)).toThrowError('LOGIN_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create LoginUser object correctly', () => {
+    const payload = {
+      username: 'abc',
+      password: 'secret',
+    };
+
+    const { username, password } = new LoginUser(payload);
+
+    expect(username).toStrictEqual(payload.username);
+    expect(password).toStrictEqual(payload.password);
+  });
+});
